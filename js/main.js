440313828f140be8a604c0ea11917dfe321c98c5
Alberto Dragoni <albertodragoni@yahoo.it>
2020-02-29 16:42:30 +0100

    done!

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
 
 
+    $( ".icon" ).mouseenter(function() {
+      $(this).next('.dropdown').slideToggle(500);
+    });
 
-
-
-
-
-
-
-
-
-
-
+    $( ".icon" ).mouseleave(function() {
+      $(this).next('.dropdown').slideToggle(500);
+    });
 
 });
